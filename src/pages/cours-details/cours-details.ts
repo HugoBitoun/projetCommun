7c22a1f9599dc38c2ad2ea31fc7be4d015f989b0
Valentin Perrin <valentin.g-a.perrin@outlook.fr>
2019-03-08 23:37:26 +0100

    Refresh is working

--- src/pages/cours-details/cours-details.ts
+++ src/pages/cours-details/cours-details.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams, Refresher} from 'ionic-angular';
+import {IonicPage, Loading, LoadingController, NavController, NavParams, Refresher} from 'ionic-angular';
 import {Cours} from "../../assets/utils/Cours";
 import {User} from "../../assets/utils/User";
 import {CoursProvider} from "../../providers/cours/cours";
@@ -27,8 +27,9 @@ export class CoursDetailsPage {
     user: User;
     listMessages: Messages[] = new Array<Messages>();
     listUsers: User[] = new Array<User>();
+    loader: Loading;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, public coursProvider: CoursProvider, public userProvider: UserProvider, public keyboard: Keyboard) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public coursProvider: CoursProvider, public loadingCtrl: LoadingController, public userProvider: UserProvider, public keyboard: Keyboard) {
         this.cours = navParams.get('cours');
         this.user = navParams.get('user');
         if (navParams.get('refresh')) {
@@ -44,14 +45,18 @@ export class CoursDetailsPage {
     }
 
     ionViewWillLoad() {
-        this.listMessages=new  Array<Messages>();
-        this.listUsers = new Array<User>();
+        this.loader = this.loadingCtrl.create({
+            content: "Patientez un peu !"
+        });
+        this.loader.present();
         this.getMessages();
     }
 
     getMessages() {
+        this.listMessages = new Array<Messages>();
         this.coursProvider.getMessagesCours(this.cours.id).then(data => {
             data.map(data => {
+
                 this.listMessages.push(data);
                 this.listMessages.sort((n1, n2) => {
                     if (n1.date > n2.date) {
@@ -66,11 +71,13 @@ export class CoursDetailsPage {
                 });
                 this.getMessagesUser();
             });
+            this.loader.dismissAll();
         });
     }
 
     getMessagesUser(event?) {
         this.userProvider.getAllUsers().subscribe(data => {
+            this.listUsers = new Array<User>();
             this.listUsers = data;
             //this.update();
         });
@@ -89,34 +96,38 @@ export class CoursDetailsPage {
         });
     }*/
     async doRefresh(refresher: Refresher) {
-
+        this.ionViewWillLoad();
         refresher.cancel();  // Works
     }
 
     getMessageColor(user: User): string {
-        if (user.roles.admin != undefined && user.roles.admin) {
-            return "bg-danger";
+        if (user != undefined) {
+            if (user.roles.admin != undefined && user.roles.admin) {
+                return "bg-danger";
+            }
+            if (user.roles.prof != undefined && user.roles.prof) {
+                return "bg-warning";
+            }
+            if (user.roles.student != undefined && user.roles.student) {
+                return "bg-info";
+            }
+            return "bg-primary";
         }
-        if (user.roles.prof != undefined && user.roles.prof) {
-            return "bg-warning";
-        }
-        if (user.roles.student != undefined && user.roles.student) {
-            return "bg-info";
-        }
-        return "bg-primary";
     }
 
     getMessageIcon(user: User): string {
-        if (user.roles.admin != undefined && user.roles.admin) {
-            return "ribbon";
+        if (user != undefined) {
+            if (user.roles.admin != undefined && user.roles.admin) {
+                return "ribbon";
+            }
+            if (user.roles.prof != undefined && user.roles.prof) {
+                return "book";
+            }
+            if (user.roles.student != undefined && user.roles.student) {
+                return "school";
+            }
+            return "logo-snapchat";
         }
-        if (user.roles.prof != undefined && user.roles.prof) {
-            return "book";
-        }
-        if (user.roles.student != undefined && user.roles.student) {
-            return "school";
-        }
-        return "logo-snapchat";
     }
 
     convertDate(date: firebase.firestore.Timestamp): string {
